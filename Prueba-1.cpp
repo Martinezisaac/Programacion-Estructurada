e11a871a90b933ffe68ecd23c7f1ef7908795043
Isaac Martinez <isaac.martinez6437@alumnos.udg.mx>
2022-01-24 12:16:42 -0600

    Prueba 1

---
+++ Prueba-1.cpp
@@ -0,0 +1,12 @@
+//ESTO ES UNA PRUEBA PARA GitHub
+
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main (){
+    cout<<"Esto es una prueba para GutHub \n" <<endl;
+    cout<<"Todo en orden amigo" <<endl;
+
+} //Fin de Main
