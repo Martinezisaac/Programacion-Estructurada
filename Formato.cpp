8430030b8a8fce52f6850697f1aac4d9be232c5f
Isaac Martinez <isaac.martinez6437@alumnos.udg.mx>
2022-01-24 20:30:12 -0600

    Formato para trabajos de programacion

---
+++ Formato.cpp
@@ -0,0 +1,22 @@
+/********************PRESENTACION*******************************
+NOMBRE: Isaac Martinez
+FECHA: 
+PROGRAMA: 
+CENTRO UNIVERSITARIO DE LOS ALTOS / UNIVERSIDAD DE GUADALAJARA
+INGENIERIA EN COMPUTACION / 2DO SEMESTRE
+PROFESOR:
+DESCRIPCION: Describir el contenido del archivo
+**************************************************************/
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+main () {
+
+    cout<<"Hola Mundo" <<endl;
+    //AQUI EMPIEZA LA MAGIA 
+
+return 0;
+}
